51627b01f82a937b805b24ca81e1ce2325d15d6d
Aleksei <leffalexx@gmail.com>
2022-10-01 13:35:43 +0300

    FaceCascedesWorking

---
+++ FaceRecognition.py
@@ -0,0 +1,17 @@
+import cv2
+
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascades.detectMultiScale(img_gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x + w, y + h), (122, 0, 0), 3)
+    cv2.imshow('Result', frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
